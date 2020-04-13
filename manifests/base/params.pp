378429d47a729745061fac30fcc57d0cabc218da
Maarten Beeckmans <maarten.beeckmans@student.hogent.be>
2020-04-13 13:06:17 +0200

    profile::base clean params class

--- manifests/base/params.pp
+++ manifests/base/params.pp
@@ -1,9 +1,7 @@
 class profile::base::params {
-  case $facts['os']['family'] {
-    Array $default_packages = ['vim-enhanced', 'tree', 'htop', 'bind-utils']
-    String $selinux_mode  = "enforcing",
-    String $selinux_type  = "targeted",
-  }
-  String $motd = "This machine is puppet managed"
-  Array $fail2ban_services = ['ssh', 'ssh-ddos']
+  Array   $default_packages   = ['vim-enhanced', 'tree', 'htop', 'bind-utils']
+  String  $selinux_mode       = "enforcing",
+  String  $selinux_type       = "targeted",
+  String  $motd               = "This machine is puppet managed"
+  Array   $fail2ban_services  = ['ssh', 'ssh-ddos']
 }
