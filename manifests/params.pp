ceb7dc2717969241078de08964fd1c7a13481fec
Maarten Beeckmans <maarten.beeckmans@student.hogent.be>
2020-04-13 21:22:12 +0200

    fix typo

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class profile::params {
   # profile::base
-  Array           $default_packages             = ['vim-enhanced', 'tree', 'htop', 'bind-utils']
+  Array           $default_packages             = ['vim-enhanced', 'tree', 'htop', 'bind-utils'],
   String          $selinux_mode                 = "enforcing",
   String          $selinux_type                 = "targeted",
   String          $motd                         = "This machine is puppet managed",
