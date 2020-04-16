d2253e59ffd467e33231d9b9e3a2d9e15fae6509
Maarten Beeckmans <maarten.beeckmans@student.hogent.be>
2020-04-16 21:33:49 +0200

    fix errors

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,20 @@
+# Class profile::params
+# =====================
+# Sets default parameters for profile class
 class profile::params {
   # profile::base
-  Array           $default_packages             = ['vim-enhanced', 'tree', 'htop', 'bind-utils'],
-  String          $selinux_mode                 = "enforcing",
-  String          $selinux_type                 = "targeted",
-  String          $motd                         = "This machine is puppet managed",
-  Array           $fail2ban_services            = ['ssh', 'ssh-ddos'],
+  $default_packages             = ['vim-enhanced', 'tree', 'htop', 'bind-utils']
+  if $facts['os']['family'] == 'RedHat' {
+    $install_epel               = true
+  } else {
+    $install_epel               = false
+  }
+  $selinux_mode                 = 'enforcing'
+  $selinux_type                 = 'targeted'
+  $motd                         = 'This machine is managed by puppet'
+  $fail2ban_services            = ['ssh', 'ssh-ddos']
 
   # profile::firewall
-  Boolean         $firewall_purge               = true,
-  Boolean         $firewall_purge_unmanaged     = true, 
+  $firewall_purge               = true
+  $firewall_purge_unmanaged     = true
 }
