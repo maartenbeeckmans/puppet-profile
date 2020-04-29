f00e0829d033b5c30c96a183f5e2d25ee49326a4
Maarten Beeckmans <maarten.beeckmans@student.hogent.be>
2020-04-29 21:36:23 +0200

    package fail2ban is managed by fail2ban module

--- manifests/base/fail2ban.pp
+++ manifests/base/fail2ban.pp
@@ -2,10 +2,6 @@ class profile::base::fail2ban (
   Array $services = ['ssh', 'ssh-ddos']
 )
 {
-  package {'fail2ban':
-    ensure => present,
-  }
-
   class { 'fail2ban':
     jails => $services
   }
